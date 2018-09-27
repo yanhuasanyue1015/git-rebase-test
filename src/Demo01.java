04860690acaa1b601038c1158f2ca5b22f63fa37
lei.zhang <1015994684@qq.com>
2018-09-27 14:08:31 +0800

    dem02

--- src/Demo01.java
+++ src/Demo01.java
@@ -1,8 +1,8 @@
 public class Demo01 {
     public static void main(String[] args) {
-        System.out.println("001");
-        System.out.println("002");
-        System.out.println("003");
-        System.out.println("004");
+        System.out.println("00001");
+        System.out.println("00002");
+        System.out.println("00003");
+        System.out.println("00004");
     }
 }
